106924393759ee3e03d8d279626d59784a94f0be
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-06 16:13:40 -0600

    Created program to find the area of a circle

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define variables
+            double area;
+            string input;
+            double radius;
+
+            //loop for wrong input
+            do
+            {
+                //ask for input
+                Console.WriteLine("What is the radius of the circle?");
+                //retrieve input
+                input = Console.ReadLine();
+                //change input type and variable name
+                radius = double.Parse(input);
+                //validation for radius
+                if (radius <= 0)
+                {
+                    Console.WriteLine("Radius cannot be negative or zero.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    //do math for area
+                    area = 3.14 * radius * radius;
+                    //write answer
+                    Console.WriteLine("The area of the circle is " + area);
+                    Console.ReadLine();
+                }
+            } while (radius <= 0);
+        }
+    }
+}
