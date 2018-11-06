147202db157aae2289902b008f51c436f0ad3f5b
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-06 16:23:35 -0600

    Created program to count characters in a string. Added the string to be counted.

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textBlock;
+
+            textBlock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nunc accumsan sem ut ligula scelerisque sollicitudin. " +
+                "Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. " +
+                "Donec sit amet suscipit metus, non lobortis massa. " +
+                "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. " +
+                "Donec nec velit non ligula efficitur luctus.";
+
+            foreach (char letter in textBlock)
+            {
+                Console.WriteLine(letter);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
