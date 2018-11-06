2bb9db77fe10468199f4657a2238b0a7d4d04f47
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-06 16:18:22 -0600

    Created program to find the area of a rectangle.

---
+++ AreaOfRectangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AreaRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double area;
+            string input_length;
+            string input_width;
+
+            Console.WriteLine("What is the length of the rectangle?");
+            input_length = Console.ReadLine();
+            length = double.Parse(input_length);
+            Console.WriteLine("What is the width of the rectangle?");
+            input_width = Console.ReadLine();
+            width = double.Parse(input_width);
+
+            area = (length * width);
+            Console.WriteLine("The area of the rectangle is " + area);
+            Console.ReadLine();
+        }
+    }
+}
