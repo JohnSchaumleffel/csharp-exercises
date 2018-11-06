3c349cdaa8611d88cda566d6fbb489af09b7e004
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-06 16:26:52 -0600

    Created a program that will ask a users name and then print hello name to the console.

---
+++ HelloName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string name;
+
+            Console.WriteLine("What is your name?");
+            input = Console.ReadLine();
+            name = (input);
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
