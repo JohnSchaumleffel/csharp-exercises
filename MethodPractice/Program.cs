40bebd3a6afab68b47c6d37683b13785045395a5
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-06 17:30:27 -0600

    Practiced with a video how to make methods.

---
+++ MethodPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Add(4, 8));
+            Add(10, 8, 2);
+            GreetUser();
+            Console.ReadLine();
+        }
+
+        public static void GreetUser ()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int Add(int numone, int numtwo)
+        {
+            return numone + numtwo;
+        }
+
+        public static void Add(int numone, int numtwo, int numthree)
+        {
+            Console.WriteLine(numone + numtwo + numthree);
+        }
+    }
+}
