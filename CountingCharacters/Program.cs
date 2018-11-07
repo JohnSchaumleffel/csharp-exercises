023de683f60584aeef1a255272ed066e8c6471da
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-07 17:22:50 -0600

    Created program to count characters in a string. Working properly.

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CountingCharacters
 {
@@ -6,22 +8,37 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
-            string textBlock;
-
-            textBlock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            //create variables
+            string textBlock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
                 "Nunc accumsan sem ut ligula scelerisque sollicitudin. " +
                 "Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. " +
                 "Donec sit amet suscipit metus, non lobortis massa. " +
                 "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. " +
                 "Donec nec velit non ligula efficitur luctus.";
 
-            foreach (char letter in textBlock)
+            string alphabet = "abcdefghijklmnopqrstuvwxyz, .ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Dictionary<char, int> LetterCount = new Dictionary<char, int>();
+
+            foreach (char c in textBlock)
             {
-                Console.WriteLine(letter);
-                Console.ReadLine();
+                if (alphabet.Contains(c))
+                {
+                    if (LetterCount.ContainsKey(c))
+                    {
+                        LetterCount[c] += 1;
+                    }
+                    else
+                    {
+                        LetterCount.Add(c, 1);
+                    }
+                }                                       
             }
-
-
+            foreach (KeyValuePair<char, int> c in LetterCount)
+            {
+                Console.WriteLine(String.Format("{0}: {1}", c.Key, c.Value));
+            }
+            Console.ReadKey();
         }
     }
 }
