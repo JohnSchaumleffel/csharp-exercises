ee79e66d8591c11057d11343983131416d2962dd
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-15 21:18:18 -0600

    Created console app to practice making classes.

---
+++ School/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Student
+    {
+        string name;
+        int studentId;
+        int numberOfCredits;
+        double gpa;
+    }
+
+    public class Course
+    {
+        Dictionary<string, double> RosterDict = new Dictionary<string, double>();
+        string courseName;
+        double creditHours;
+        
+        
+    }
+}
