437c0afb038d8fd2571dacfc5e78e231cd5bb2fb
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-29 19:28:42 -0600

    Updated Area of Rectangle to run with classes.

--- AreaOfRectangle/Program.cs
+++ AreaOfRectangle/Program.cs
@@ -6,22 +6,31 @@ namespace AreaRectangle
     {
         static void Main(string[] args)
         {
-            double length;
-            double width;
-            double area;
-            string input_length;
-            string input_width;
+            AreaOfRectangle rect = new AreaOfRectangle();
+            rect.ComputeArea();
+        }
+            public class AreaOfRectangle
+            {
+                double length;
+                double width;
+                double area;
+                string input_length;
+                string input_width;
 
-            Console.WriteLine("What is the length of the rectangle?");
-            input_length = Console.ReadLine();
-            length = double.Parse(input_length);
-            Console.WriteLine("What is the width of the rectangle?");
-            input_width = Console.ReadLine();
-            width = double.Parse(input_width);
+                public void ComputeArea()
+                {
+                    Console.WriteLine("What is the length of the rectangle?");
+                    input_length = Console.ReadLine();
+                    length = double.Parse(input_length);
+                    Console.WriteLine("What is the width of the rectangle?");
+                    input_width = Console.ReadLine();
+                    width = double.Parse(input_width);
 
-            area = (length * width);
-            Console.WriteLine("The area of the rectangle is " + area);
-            Console.ReadLine();
+                    area = (length * width);
+                    Console.WriteLine("The area of the rectangle is " + area);
+                    Console.ReadLine();
+                }
+            }
         }
     }
-}
+
