2735340521a636139ebc309b6129c1af92829cb3
johnschaumleffel <johnschaumleffel@gmail.com>
2018-11-29 19:37:28 -0600

    Updated area of circle to use classes.

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -6,35 +6,46 @@ namespace Area
     {
         static void Main(string[] args)
         {
+            AreaOfCircle circ = new AreaOfCircle();
+            circ.ComputeArea();
+        }
+
+            public class AreaOfCircle
+        {
             //define variables
             double area;
             string input;
             double radius;
 
-            //loop for wrong input
-            do
+            public void ComputeArea()
             {
-                //ask for input
-                Console.WriteLine("What is the radius of the circle?");
-                //retrieve input
-                input = Console.ReadLine();
-                //change input type and variable name
-                radius = double.Parse(input);
-                //validation for radius
-                if (radius <= 0)
+                //loop for wrong input
+                do
                 {
-                    Console.WriteLine("Radius cannot be negative or zero.");
-                    Console.ReadLine();
-                }
-                else
-                {
-                    //do math for area
-                    area = 3.14 * radius * radius;
-                    //write answer
-                    Console.WriteLine("The area of the circle is " + area);
-                    Console.ReadLine();
-                }
-            } while (radius <= 0);
+                    //ask for input
+                    Console.WriteLine("What is the radius of the circle?");
+                    //retrieve input
+                    input = Console.ReadLine();
+                    //change input type and variable name
+                    radius = double.Parse(input);
+                    //validation for radius
+                    if (radius <= 0)
+                    {
+                        Console.WriteLine("Radius cannot be negative or zero.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        //do math for area
+                        area = 3.14 * radius * radius;
+                        //write answer
+                        Console.WriteLine("The area of the circle is " + area);
+                        Console.ReadLine();
+                    }
+                } while (radius <= 0);
+            }
+        }
+
         }
     }
-}
+
