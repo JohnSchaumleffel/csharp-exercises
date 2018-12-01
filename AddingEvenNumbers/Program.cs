455be754bf2167be55f7ded3da60e19277aeac16
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-01 17:44:30 -0600

    Created console app to create a list of numbers and add all the even integers.

---
+++ AddingEvenNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingEvenNumbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> integers = SetupIntegerList();
+            int sum = GetSumOfEvenIntegers(integers);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        public static int GetSumOfEvenIntegers(List<int> integers)
+        {
+            int sum = 0;
+            foreach(int integer in integers)
+            {
+                if (integer % 2 == 0)
+                    sum += integer;
+            }
+
+            return sum;
+        }
+
+        public static List<int> SetupIntegerList()
+        {
+            List<int> integers = new List<int>();
+            for (int i = 0; i < 30; i++)
+                integers.Add(i);
+            return integers;
+        }
+
+    }
+}
+
