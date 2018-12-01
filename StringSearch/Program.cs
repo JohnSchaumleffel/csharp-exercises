213fef7e0f4168346e588621d2886dba8ed7717d
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-01 15:11:46 -0600

    Updated string search to use classes.

--- StringSearch/Program.cs
+++ StringSearch/Program.cs
@@ -6,27 +6,42 @@ namespace StringSearch
     {
         static void Main(string[] args)
         {
+            StringSearch search = new StringSearch();
+            search.SearchString();
+        }
+
+        public class StringSearch
+        {
             string source_material;
             string search_phrase;
             string lc_source_material;
             string lc_search_phrase;
 
-            source_material = ("Alice was beginning to get very tired of sitting " +
+
+        
+
+            public void SearchString()
+            {
+                source_material = ("Alice was beginning to get very tired of sitting " +
                 "by her sister on the bank, and of having nothing to do: once or twice " +
                 "she had peeped into the book her sister was reading, but it had no " +
                 "pictures or conversations in it, 'and what is the use of a book,' " +
                 "thought Alice 'without pictures or conversation?'");
-            lc_source_material = source_material.ToLower();
 
-            Console.WriteLine("What phrase would you like to search for?");
-            search_phrase = Console.ReadLine();
-            lc_search_phrase = search_phrase.ToLower();
+                lc_source_material = source_material.ToLower();
 
-            Console.WriteLine(lc_source_material.Contains(lc_search_phrase));
-            Console.ReadLine();
+                Console.WriteLine("What phrase would you like to search for?");
+                search_phrase = Console.ReadLine();
+                lc_search_phrase = search_phrase.ToLower();
+
+                Console.WriteLine(lc_source_material.Contains(lc_search_phrase));
+                Console.ReadLine();
 
 
-            //functions properly
+                //functions properly
+            }
+
         }
     }
 }
+
