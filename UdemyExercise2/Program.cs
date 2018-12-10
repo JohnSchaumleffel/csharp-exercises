5a684bc0adb4ec1a104625f5633f61630d4ba2c0
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-09 23:38:38 -0600

    Completed Udemy exercise two.

---
+++ UdemyExercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Please enter a number.");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number.");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
