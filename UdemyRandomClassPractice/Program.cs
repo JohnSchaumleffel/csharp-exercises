aa2e0ee246ff2a59bd68b22f718be8485214d352
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-10 13:43:35 -0600

    Completed Udemy random class practice.

---
+++ UdemyRandomClassPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyRandomClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordLength = 10;
+
+            char[] buffer = new char[passwordLength];
+
+            for(var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
