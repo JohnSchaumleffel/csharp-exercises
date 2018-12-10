1e54153fbe38b2ef666cebca15c74f46d771e22c
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-09 23:33:31 -0600

    Completed Udemy exercise one.

---
+++ UdemyExercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please choose a number between 1 and 10.");
+            number = int.Parse(Console.ReadLine());
+
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+
+        }
+    }
+}
