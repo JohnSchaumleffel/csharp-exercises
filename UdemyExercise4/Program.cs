2166c5ded82a3602c3b7a69b2601b8ad70bd1d96
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-10 00:03:39 -0600

    Completed Udemy exercise four.

---
+++ UdemyExercise4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speedLimit;
+            int vehicleSpeed;
+
+            Console.WriteLine("What is the speed limit?");
+            speedLimit = int.Parse(Console.ReadLine());
+            Console.WriteLine("How fast is the car going");
+            vehicleSpeed = int.Parse(Console.ReadLine());
+
+            if (vehicleSpeed <= speedLimit)
+            {
+                Console.WriteLine("Good Job!");
+            }
+
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (vehicleSpeed - speedLimit) / kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                    Console.WriteLine("License Suspended");
+                else
+                    Console.WriteLine("Demerit Points:" + demeritPoints);
+            }
+        }
+    }
+}
