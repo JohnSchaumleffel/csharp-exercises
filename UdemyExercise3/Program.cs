b317caf9520f66b3b92570dc998ee76953713a44
johnschaumleffel <johnschaumleffel@gmail.com>
2018-12-09 23:43:25 -0600

    Completed Udemy exercise three.

---
+++ UdemyExercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            int width;
+
+            Console.WriteLine("Please enter the height of the image.");
+            height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the width of the image.");
+            width = int.Parse(Console.ReadLine());
+
+            if (height > width)
+            {
+                Console.WriteLine("The image is a portrait.");
+            }
+
+            else
+            {
+                Console.WriteLine("The image is a landscape.");
+            }
+        }
+    }
+}
